5e9d0ca5194084c363f603370e99f4593affe607
Kushagra Seth <kseth919@gmail.com>
2018-08-20 03:16:04 +0530

    Binarized Data

--- Prepare_Data_for_ML/Binarize_Data.py
+++ Prepare_Data_for_ML/Binarize_Data.py
@@ -9,11 +9,10 @@ data = pandas.read_csv(rawData, names=colNames)
 array = data.values
 X = array[:, :-1]
 Y = array[:, -1]
-scaler = skp.StandardScaler()
-rescaledX = scaler.fit_transform(X)
+binarizer = skp.Binarizer(threshold=0.0)
+rescaledX = binarizer.fit_transform(X)
 numpy.set_printoptions(precision=3)
-print("Standardized Input Features(first 5 rows & all columns): ")
+print("Binarized Input Features(first 5 rows & all columns): ")
 print("-----------------------------------------------------------")
 print(rescaledX[0:5, :])
 print("-----------------------------------------------------------")
-
